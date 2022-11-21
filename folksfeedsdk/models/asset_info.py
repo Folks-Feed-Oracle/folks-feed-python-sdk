8ce273a1943ace1fe3d6c0efc4e4d5ff4588920a
palace22 <palazzolo1995@gmail.com>
2022-11-21 17:18:33 +0100

    update data position in gs

--- folksfeedsdk/models/asset_info.py
+++ folksfeedsdk/models/asset_info.py
@@ -13,8 +13,8 @@ class AssetInfo:
     @classmethod
     def from_global_state(cls, name, decimals, gs):
         asset_name = name
-        price_threshold = round(gs_to_int(gs, 0) / 1e4, 2)
-        raw_price = gs_to_int(gs, 1)
-        price = gs_to_int(gs, 1) * (10**decimals / 1e14)
-        latest_timestamp = gs_to_int(gs, 2)
+        price_threshold = round(gs_to_int(gs, 2) / 1e4, 2)
+        raw_price = gs_to_int(gs, 0)
+        price = raw_price * (10**decimals / 1e14)
+        latest_timestamp = gs_to_int(gs, 1)
         return cls(asset_name, price_threshold, raw_price, price, latest_timestamp)
