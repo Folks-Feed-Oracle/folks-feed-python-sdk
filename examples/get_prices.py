a86998ccd0bcfd9cad576390ab9bfd1384b96923
palace22 <palazzolo1995@gmail.com>
2023-03-09 14:42:55 +0100

    update get asset info

--- examples/get_prices.py
+++ examples/get_prices.py
@@ -1,4 +1,5 @@
 from algosdk.v2client.algod import AlgodClient
+from algosdk.v2client.indexer import IndexerClient
 from folksfeedsdk.folks_feed_client import FolksFeedClient
 from folksfeedsdk.constants import OracleAppId, TestnetAssetId, MainnetAssetId
 from folksfeedsdk.models.asset_info import AssetInfo
@@ -7,23 +8,26 @@ network = "mainnet"
 
 if network == "mainnet":
     algod_address = "https://node.algoexplorerapi.io"
+    indexer_address = "https://algoindexer.algoexplorerapi.io/"
     oracle_app_id = OracleAppId.MAINNET
     asset_id = MainnetAssetId
 else:
     algod_address = "https://node.testnet.algoexplorerapi.io"
+    indexer_address = "https://algoindexer.testnet.algoexplorerapi.io/"
     oracle_app_id = OracleAppId.TESTNET
     asset_id = TestnetAssetId
 
 
 algod_client = AlgodClient("", algod_address)
+indexer_client = IndexerClient("", indexer_address)
 
 
-ffo_client = FolksFeedClient(algod_client, oracle_app_id)
+ffo_client = FolksFeedClient(algod_client, indexer_client, oracle_app_id)
 
 algo_info: AssetInfo = ffo_client.get_asset_info(asset_id.ALGO)
 print(algo_info.__dict__)
 
-goETH_info: AssetInfo = ffo_client.get_asset_info(asset_id.OPUL)
+goETH_info: AssetInfo = ffo_client.get_asset_info(asset_id.OPUL, 27520504)
 print(goETH_info.__dict__)
 
 goBTC_price = ffo_client.get_asset_price(asset_id.goBTC)
--- folksfeedsdk/folks_feed_client.py
+++ folksfeedsdk/folks_feed_client.py
@@ -1,3 +1,4 @@
+from algosdk.v2client.indexer import IndexerClient
 from algosdk.v2client.algod import AlgodClient
 from folksfeedsdk.constants import ALGO_PARAMS
 from folksfeedsdk.utils import gs_to_int
@@ -5,14 +6,28 @@ from folksfeedsdk.models.asset_info import AssetInfo
 
 
 class FolksFeedClient:
-    def __init__(self, algod_client: AlgodClient, app_id: int):
+    def __init__(self, algod_client: AlgodClient, indexer_client: IndexerClient, app_id: int):
         self.algod_client = algod_client
+        self.indexer_client = indexer_client
         self.app_id = app_id
 
-    def get_asset_info(self, asset_id: int):
+    def get_global_states(self, round_num):
+        return (
+            self.algod_client.application_info(self.app_id)["params"]["global-state"]
+            if not round_num
+            else self.indexer_client.search_transactions(
+                limit=1,
+                sig_type="sig",
+                min_round=round_num - 20,
+                max_round=round_num,
+                application_id=self.app_id,
+            )["transactions"][0]["global-state-delta"]
+        )
+
+    def get_asset_info(self, asset_id: int, round_num: int = None):
         asset_info_params = self.algod_client.asset_info(asset_id)["params"] if asset_id else ALGO_PARAMS
         name, decimals = asset_info_params["unit-name"], asset_info_params["decimals"]
-        global_states = self.algod_client.application_info(self.app_id)["params"]["global-state"]
+        global_states = self.get_global_states(round_num)
         for global_state in global_states:
             try:
                 if gs_to_int(global_state["key"], 0) == asset_id:
